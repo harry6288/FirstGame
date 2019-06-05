2331d3bf90927771f9640985bd75f35bcc686103
hardeepkaur <hardeepwalia019@gmail.com>
2019-06-05 09:56:57 -0400

    add Square

--- FirstGame/GameScene.swift
+++ FirstGame/GameScene.swift
@@ -11,11 +11,32 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-   
+   override func didMove(to view: SKView) {
+    // GameEngine constructor from android
     
-    override func didMove(to view: SKView) {
-        
-        
+    //create label object
+     let scoreLabel = SKLabelNode(text: "HELLO WORLD")
+    //set the label position
+    scoreLabel.position = CGPoint(x:100 , y: 400)
+    
+    // change font and color of text
+    
+    scoreLabel.fontName = "Avenir"
+    scoreLabel.fontSize = 45
+    scoreLabel.fontColor = SKColor.yellow
+    
+    
+    // add the label to scene
+    addChild(scoreLabel)
+    
+    //// add square
+    
+    let square = SKSpriteNode(color: SKColor.red, size: CGSize(width: 200, height: 200))
+     square.anchorPoint = CGPoint(x: 1, y: 1)
+    
+    square.position = CGPoint(x: 300, y: 350)
+    addChild(square)
+    
     }
 
 }
